ccb434607187e93ec41373fa7ab64a9f2e872ac4
paxington181 <tsandrus@rogers.com>
2025-04-05 18:59:20 -0400

    It works...sorta.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ title:str = "Word Raider"
 words:str = []
 wrong_place:str = []
 wrong_let:str = []
-turn_number:int = 0
+
 max_turn:int = 5
 solution:str = " "
 
@@ -15,11 +15,28 @@ def main():
         for word in wordlist:
             words.append(word.rstrip().lower())
     solution = random.choice(words)
-
-    print(f"Your word has {len(solution)} letters!  Good luck!")
-    print(f"You have {max_turn - turn_number} turns remaining to guess the word.")
-    while turn_number <= max_turn:
-        #building gameplay loop https://app.dataquest.io/c/93/m/855/guided-project%3A-word-raider/6/validate-the-guessed-word
     print(solution)
+    print(f"Your word has {len(solution)} letters!  Good luck!")
+    turn_number:int = 0
+    while turn_number < (max_turn + 1):
+        if turn_number == 5:
+            print("Game over!")
+            break
+        elif turn_number == 4:
+            print("Last turn!")
+        else:
+            print(f"You have {max_turn - turn_number} turns remaining to guess the word.")
+        guess = input(f"Please enter your {len(solution)} letter guess: ")
+        guess.lower()
+        if len(guess) != len(solution):
+            print("Incorrect word length, try again")
+        elif guess.isalpha() is False:
+            print("Please enter only letters.")
+        elif guess == solution:
+            print("Correct! You win!")
+            break
+        else:
+            turn_number += 1
+    
 
 main()
